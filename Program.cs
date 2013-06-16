c8829e57c5e5a7006512f9811259c92238d14786
Mohammed Hossain <mohammedh123@gmail.com>
2013-06-15 21:09:38 -0400

    Deletes and adds working startup code.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjectPsi
+{
+    class Program
+    {
+#if NETFX_CORE
+        [MTAThread]
+#else
+        [STAThread]
+#endif
+        static void Main()
+        {
+            using (var program = new PsiGame())
+                program.Run();
+        }
+    }
+}
