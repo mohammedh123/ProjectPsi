ec327240c886ef0237bc95cd0a776db37c758614
Mohammed Hossain <mohammedh123@gmail.com>
2014-01-28 23:49:30 -0500

    Adds a basic tile map class and a MathHelper class to the Core project.

---
+++ ProjectPsi.Core/Map.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjectPsi.Core
+{
+    public class Map
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int[,] Tiles { get; set; }
+
+        public float TileRadius { get; set; }
+
+        public Map(int width, int height, float tileRadius)
+        {
+            Width = width;
+            Height = height;
+            TileRadius = tileRadius;
+
+            Tiles = new int[height, width];
+
+            for (var i = 0; i < height; ++i) {
+                for (var j = 0; j < width; ++j) {
+                    Tiles[i, j] = -1;
+                }
+            }
+        }
+
+        public void SetTile(int x, int y, int spriteIndex)
+        {
+            Tiles[y, x] = spriteIndex;
+        }
+    }
+}
