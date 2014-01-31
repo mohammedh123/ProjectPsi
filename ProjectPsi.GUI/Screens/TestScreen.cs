343ae104340da61702d9c6feb9015100d120847d
Mohammed Hossain <mohammedh123@gmail.com>
2014-01-31 18:34:00 -0500

    Moves the test logic/drawing to the test screen from the main game class.

--- ProjectPsi.GUI/Screens/TestScreen.cs
+++ ProjectPsi.GUI/Screens/TestScreen.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectPsi.Core;
+using SFML.Graphics;
+using SFML.Window;
 
 namespace ProjectPsi.GUI.Screens
 {
     class TestScreen : GameScreen
     {
+        private Map _map;
+        private List<Sprite> _tileSprites;
+        private View _mapView;
+        
+        public override void LoadContent()
+        {
+            Game.TextureManager.LoadTexture("tilemap", @"Resources/Textures/tilemap.png");
+
+            var tileSprite = new Sprite(Game.TextureManager.GetTexture("tilemap"), new IntRect(0, 0, 64, 64));
+            tileSprite.Origin = new Vector2f(32, 32);
+
+            _tileSprites = new List<Sprite>();
+            _tileSprites.Add(tileSprite);
+
+            _map = new Map(18, 15, 31);
+
+            for (int r = 0; r < _map.Height; r++) {
+                for (int c = 0; c < _map.Width; c++) {
+                    if (r == 0 || c == 0 || r == _map.Height - 1 || c == _map.Width - 1) {
+                        _map.SetTile(c, r, 0);
+                    }
+                }
+            }
+
+            _mapView = Game.Window.GetView();
+        }
+
         public override void Update(TimeSpan gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
         {
             base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
@@ -15,7 +45,40 @@ namespace ProjectPsi.GUI.Screens
 
         public override void Draw(TimeSpan gameTime)
         {
-            base.Draw(gameTime);
+            Game.Window.Clear();
+
+            DrawMap(Game.Window);
+        }
+
+        private void DrawMap(RenderWindow window)
+        {
+            var tileWidth = _map.TileRadius * 2;
+            var tileHeight = (float)(tileWidth * Math.Sin(Math.PI / 3));
+
+            for (var row = 0; row < _map.Height; row++)
+            {
+                for (var col = 0; col < _map.Width; col++)
+                {
+                    //even-q vertical layout
+
+                    var spriteIdx = _map.Tiles[row, col];
+                    if (spriteIdx < 0)
+                    {
+                        continue;
+                    }
+
+                    var xPos = col * tileWidth * 0.75f;
+                    var yPos = row * tileHeight;
+
+                    if (col % 2 != 0)
+                    {
+                        yPos -= tileHeight * 0.5f;
+                    }
+
+                    _tileSprites[spriteIdx].Position = new Vector2f(xPos, yPos);
+                    window.Draw(_tileSprites[spriteIdx]);
+                }
+            }
         }
     }
 }
