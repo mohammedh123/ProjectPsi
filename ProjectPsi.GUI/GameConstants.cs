6644ddd4dbeb1225dcbe87ac452a835a41d8b34e
Mohammed Hossain <mohammedh123@gmail.com>
2014-02-05 01:30:49 -0500

    Adds a constant for screen panning speed.

---
+++ ProjectPsi.GUI/GameConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPsi.GUI
+{
+    static class GameConstants
+    {
+        /// <summary>
+        /// Panning speed in pixels per second.
+        /// </summary>
+        public static float PanningSpeed = 150f;
+    }
+}
